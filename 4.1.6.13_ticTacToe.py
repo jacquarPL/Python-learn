35b15fb8b41f3a59b333eb34ab2c63fe2d9e3912
jacquarPL <jacquar@gmail.com>
2020-07-21 20:25:34 +0200

    Create 4.1.6.13_ticTacToe.py

---
+++ 4.1.6.13_ticTacToe.py
@@ -0,0 +1,42 @@
+def display_board(board):
+#
+# Funkcja, która przyjmuje jeden parametr zawierający bieżący stan tablicy
+# i wyświetla go w oknie konsoli.
+#    
+    for rows in board:
+        print("+-------+-------+-------+")
+        print("|       |       |       |")
+        for field in rows:
+            print("|  ",field, "  ", end="")
+        print("|")
+        print("|       |       |       |")
+    print("+-------+-------+-------+")
+
+def enter_move(board):
+    board[1][0] = "o"
+#
+# Funkcja, która przyjmuje parametr odzwierciedlający biężący stan tablicy,
+# prosi użytkownika o wykonanie ruchu, 
+# sprawdza dane wejściowe i aktualizuje tablicę zgodnie z decyzją użytkownika.
+#
+def make_list_of_free_fields(board):
+    a=1
+#
+# Funkcja, która przegląda tablicę i tworzy listę wszystkich wolnych pól; 
+# lista składa się z krotek, a każda krotka zawiera parę liczb odzwierciedlających rząd i kolumnę.
+#
+def victory_for(board, sign):
+    a=1
+#
+# Funkcja, która dokonuje analizy stanu tablicy w celu sprawdzenia
+# czy użytkownik/gracz stosujący "O" lub "X" wygrał rozgrywkę.
+#
+def draw_move(board):
+    a=1
+#
+# Funkcja, która wykonuje ruch za komputer i aktualizuje tablicę.
+#
+board = [[1,2,3],[4,"x",6],[7,8,9]]
+display_board(board)
+enter_move(board)
+display_board(board)
