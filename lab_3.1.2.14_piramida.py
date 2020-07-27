2efe571dd669a4924fa4b52f770a4261e6084131
jacquarPL <jacquar@gmail.com>
2020-07-27 16:45:28 +0200

    Add files via upload

---
+++ lab_3.1.2.14_piramida.py
@@ -0,0 +1,11 @@
+﻿#blokow = int(input("Wprowadź liczbę bloków: "))
+blokow = 2
+#
+# Napisz tutaj swój kod.
+#	
+wysokosc = 0
+while blokow > wysokosc:
+    for i in range(wysokosc +1):
+        blokow -= 1
+    wysokosc += 1
+print("Wysokość piramidy wynosi:", wysokosc)
