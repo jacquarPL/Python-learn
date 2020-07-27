295184cc456f536e4888e86c1f3763d6748155b5
jacquarPL <jacquar@gmail.com>
2020-07-27 17:11:27 +0200

    Delete forMissisipi.py

--- forMissisipi.py
+++
@@ -1,9 +0,0 @@
-﻿import time # LINIA I
-
-for i in range(1,6):
-# Napisz pętlę for, która liczy do pięciu.
-    # Ciało pętli: wyświetl w oknie konsoli numer iteracji i słowo "Mississippi".
-    time.sleep(1) # LINIA II
-    print(i, " Missisipi")
-# Napisz funkcję print, która wyświetli wiadomość końcową.
-print("Ready or not, here I come!")
